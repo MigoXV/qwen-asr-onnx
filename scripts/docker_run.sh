ea04f7393dc09eb4151274915f57ce23cbf58f9d
migo <1695587905@qq.com>
2026-05-11 16:54:03 +0800

    Merge branch 'fix/docker'

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,5 +1,15 @@
 docker run -it --rm \
 -p 50051:50051 \
--e QWEN_ASR_CONFIG=examples/qwen-asr-a733/config.yaml \
+-e CONFIG_PATH=examples/qwen-asr-onnx/config.yaml \
+-e MODEL_PATH=model-bin/Daumee/Qwen3-ASR-0.6B-ONNX-CPU \
 -v ${PWD}/model-bin:/app/model-bin \
-registry.cn-hangzhou.aliyuncs.com/migo-dl/qwen-asr-onnx:0.1.0a1-amd64
+registry.cn-hangzhou.aliyuncs.com/migo-dl/qwen-asr-onnx:0.1.0a5-amd64
+
+# 213
+docker run -it --rm \
+--name wcw_qwen-asr-onnx \
+-p 50015:50051 \
+-e CONFIG_PATH=examples/qwen-asr-onnx/config.yaml \
+-e MODEL_PATH=/model-bin/Qwen3-ASR-0.6B-ONNX-CPU \
+-v ${PWD}:/model-bin \
+registry.cn-hangzhou.aliyuncs.com/migo-dl/qwen-asr-onnx:0.1.0a5-amd64
